e8dfdab19f3fea357cdcb1f2ea8ee12d2eb6872f
KejDhruv-Pharbit <dhruv_kejriwal@pharbit.com>
2026-02-06 17:45:00 +0530

    Routing

---
+++ Backend/Routes/Users/Organization.js
@@ -0,0 +1,53 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import { createOrgWallet } from "../../Database/Users/Organization/AddOrganization.js";
+
+dotenv.config();
+const router = express.Router();
+router.use(express.json());
+router.use(cookieParser());
+
+
+router.post("/organization/entry", async (req, res) => {
+  try {
+    const {
+      registrationId,
+      organizationName,
+      type
+    } = req.body;
+      
+    if (!registrationId || !organizationName || !type) {
+      return res.status(400).json({
+        message: "registrationId, organizationName and type are required"
+      });
+    }
+      
+    const org = await createOrgWallet({
+      registrationId,
+      organizationName,
+      type
+    });
+
+    return res.status(201).json({
+      message: "Organization registered successfully",
+      organization: {
+        id: org.id,
+        name: org.name,
+        registrationId,
+        type,
+        walletAddress: org.address,
+        privateKey: org.privateKey
+      }
+    });
+
+  } catch (err) {
+    console.error("Org signup error:", err);
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+});
+
+export default router;
---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+
+import Organization from "./Routes/Users/Organization.js"
+dotenv.config();
+
+const app = express();
+const PORT = 5000;
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use("/", Organization); 
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
