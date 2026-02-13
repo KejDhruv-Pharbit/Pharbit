143b5c713b799f7467c3cd575c38eb991b328d10
KejDhruv-Pharbit <dhruv_kejriwal@pharbit.com>
2026-02-13 15:29:57 +0530

    Intialzing Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+         <Route index element={<h1>Hello</h1>} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
