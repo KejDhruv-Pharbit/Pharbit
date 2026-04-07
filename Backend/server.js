b0fd217777baca8a8b118837dfdb605a90a2f3d7
KejDhruv-Pharbit <dhruv_kejriwal@pharbit.com>
2026-04-07 18:01:43 +0530

    yay

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ const PORT = 6090;
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5174",
   credentials: true
 }));
 // Only parse JSON
